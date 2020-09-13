9d243eeec142a08e3c6a782ac77fa0f72f08c4eb
Corey <coreyborad@hotmail.com.tw>
2020-09-13 18:52:57 +0800

    [add] add vuetify

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <div class="mr-4">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+            <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
+          </v-form>
+        </div>
+      </v-container>
+    </v-main>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  },
+  data (){
+    return {
+      input: this.input
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
