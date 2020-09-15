d4bb126e04ebee692830c0089e8daf8e4ff82fbb
Corey <coreyborad@hotmail.com.tw>
2020-09-15 22:11:10 +0800

    [fix] fix lint

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -4,14 +4,14 @@
       <v-container fluid>
         <div class="mr-4">
           <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
-            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+            <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required />
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required />
             <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
           </v-form>
         </div>
       </v-container>
     </v-main>
-    <v-footer app></v-footer>
+    <v-footer app />
   </v-app>
 </template>
 
@@ -19,9 +19,8 @@
 export default {
   name: 'Login',
   props: {
-    msg: String
   },
-  data (){
+  data() {
     return {
       input: this.input
     }
