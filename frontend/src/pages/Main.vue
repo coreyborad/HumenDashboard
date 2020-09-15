2d883ee5236acea39753e6baae703a061605d63a
Corey <coreyborad@hotmail.com.tw>
2020-09-15 22:11:10 +0800

    [fix] fix lint

--- frontend/src/pages/Main.vue
+++ frontend/src/pages/Main.vue
@@ -1,26 +1,26 @@
 <template>
-<v-app>
-  <v-navigation-drawer app>
+  <v-app>
+    <v-navigation-drawer app>
     <!-- -->
-  </v-navigation-drawer>
+    </v-navigation-drawer>
 
-  <v-app-bar app>
+    <v-app-bar app>
     <!-- -->
-  </v-app-bar>
+    </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-main>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
 
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
+        <!-- If using vue-router -->
+        <router-view />
+      </v-container>
+    </v-main>
 
-  <v-footer app>
+    <v-footer app>
     <!-- -->
-  </v-footer>
-</v-app>
+    </v-footer>
+  </v-app>
 </template>
