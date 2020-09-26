f58b9ff2ec907408119b3ff7831b0e4a5de237a7
Corey <coreyliu@weintek.com>
2020-09-26 17:46:15 +0800

    [add] add main page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../pages/Login.vue'
+import MainLayout from '../pages/MainLayout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/',
+    name: 'MainLayout',
+    meta: { title: 'Main' },
+    component: MainLayout,
+    children: [
+      {
+        path: 'summary',
+        name: 'Summary',
+        meta: { title: 'Summary', icon: 'summary' }
+      },
+      {
+        path: 'my_stock',
+        name: 'My Stock',
+        meta: { title: 'My Stock', icon: 'stock' }
+      }
+    ]
   }
 ]
 
