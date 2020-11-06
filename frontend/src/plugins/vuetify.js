678bff0117414c21f601c84cdf14f4b0072e2450
Corey <coreyliu@weintek.com>
2020-11-06 11:35:33 +0800

    [add] add v-icon

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -7,6 +8,9 @@ Vue.use(Vuetify)
 const opts = {
   theme: {
     dark: true
+  },
+  icons: {
+    iconfont: 'mdi' // default - only for display purposes
   }
 }
 
