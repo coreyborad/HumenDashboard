1c1a24a0192d072fbbce25ba30f76e0236d72eff
Corey <coreyborad@hotmail.com.tw>
2021-12-17 18:03:37 +0800

    [PWA] Draft on 2021/12/17

--- pwa/xlsx-form/src/App.tsx
+++ pwa/xlsx-form/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Login } from './Login/Main'
 import { Form } from './Form/Main'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Form></Form>
+      <Route path="/" component={Login} />
+      <Route path="/form" component={Form} />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
